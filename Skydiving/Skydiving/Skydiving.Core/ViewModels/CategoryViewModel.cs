b9b857e73fc405fdaf57f4feecc291a01e9c96ae
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-03-09 23:14:25 +0200

    restart project

--- Skydiving/Skydiving/Skydiving.Core/ViewModels/CategoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Skydiving.Core.ViewModels
-{
-    public class CategoryViewModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; } = null!;
-    }
-}
--- Skydiving/Skydiving/Skydiving.Core/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Skydiving.Core.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
