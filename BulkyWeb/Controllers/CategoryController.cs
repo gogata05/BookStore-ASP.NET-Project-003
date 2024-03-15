42223dbb7cfa4feb63da090262a4d34c0734d8da
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-03-15 06:32:32 +0200

    Update Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -46,8 +46,8 @@ namespace BulkyWeb.Controllers
                 return NotFound();
             }
             Category? categoryFromDb = _db.Categories.Find(id);
-            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
 
             if (categoryFromDb == null)
             {
@@ -58,14 +58,9 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-            }
-
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
