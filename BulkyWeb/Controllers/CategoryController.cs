9259519579e4f76675ec31045f08022725a97614
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-03-15 06:28:27 +0200

    Edit and Delete Buttons

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,7 +21,6 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult Create(Category obj)
         {
@@ -29,6 +28,7 @@ namespace BulkyWeb.Controllers
             {
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
             }
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -36,6 +36,41 @@ namespace BulkyWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
