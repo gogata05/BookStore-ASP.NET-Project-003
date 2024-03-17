bbf4561a39898a67ffd540de893030a84dcd3e4a
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-03-17 23:33:46 +0200

    Add Foreign Key in EF Core

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -1,5 +1,4 @@
-﻿using BulkyBook.DataAccess.Data;
-using BulkyBook.DataAccess.Repository.IRepository;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using System;
 using System.Collections.Generic;
@@ -7,6 +6,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using BulkyBook.DataAccess.Data;
 
 namespace BulkyBook.DataAccess.Repository
 {
