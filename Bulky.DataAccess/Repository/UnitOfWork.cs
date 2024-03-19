57f85375f69f719d9bd8a02c2a9b964b029df7a5
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-03-19 11:01:13 +0200

    Company CRUD operations added

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -1,10 +1,10 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.DataAccess.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BulkyBook.DataAccess.Data;
 
 namespace BulkyBook.DataAccess.Repository
 {
@@ -12,13 +12,14 @@ namespace BulkyBook.DataAccess.Repository
     {
         private ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
+        public ICompanyRepository Company { get; private set; }
         public IProductRepository Product { get; private set; }
-
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);   
         }
 
         public void Save()
