80e298c325d6c56a996c969e484b0b8bbfebbe7a
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-03-20 16:06:07 +0200

    Create: OrderHeader, OrderDetails Repository and UnitOfWork update

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -16,6 +16,8 @@ namespace BulkyBook.DataAccess.Repository
         public IProductRepository Product { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -24,7 +26,9 @@ namespace BulkyBook.DataAccess.Repository
             ShoppingCart = new ShoppingCartRepository(_db);
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
-            Company = new CompanyRepository(_db);   
+            Company = new CompanyRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public void Save()
